a852f710180733c9783b0d271ae3f1d9daa6e246
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2023-11-11 19:17:50 +0530

    some changes

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -24,7 +24,7 @@ const signup = async (req, res) => {
     try {
         const existingUser = await userModel.findOne({ email: email });
         if (existingUser) {
-            return res.status(400).json({ message: "User already exists" }); // 400 means bad request
+            return res.status(400).json({ message: "User already exists" }); 
         }
 
         const verificationToken = jwt.sign({ email }, SECRET_KEY, { expiresIn: '1d' });
